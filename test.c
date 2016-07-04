fc64e25c0f3b0f9d301420de50f128fe9d960484
Jeffrey Crowell <crowell@bu.edu>
2016-07-04 14:05:27 -0400

    add example neural net and train/test programs

---
+++ test.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "floatfann.h"
+
+int nn_2_val(fann_type* out) {
+	fann_type max = 0;
+	int maxidx = 0;
+	int i = 0;
+	for (i = 0; i < 10; ++i) {
+		if (out[i] > max) {
+			maxidx = i;
+			max = out[i];
+		}
+	}
+	return maxidx;
+}
+
+int main(void) {
+	fann_type *calc_out;
+	fann_type input[784];
+
+	struct fann *ann = fann_create_from_file("numbers.net");
+	FILE* fh;
+	fh = fopen("./nntest.nn", "r");
+	if (fh == NULL){
+		printf("file doesn't exist!\n");
+		return 0;
+	}
+	//read line by line
+	const size_t line_size = 8192;
+	char* line = malloc(line_size);
+	int i = 0;
+	int val;
+	int gt;
+	int got = 0;
+	while (fgets(line, line_size, fh) != NULL)  {
+		++i;
+		if (i == 1) {
+			continue;
+		}
+		char* pch;
+		int j = 0;
+		pch = strtok(line, " ");
+		while (pch != NULL) {
+			input[j++] = atof(pch);
+			pch = strtok(NULL, " ");
+		}
+		if (i % 2 == 0) {
+			calc_out = fann_run(ann, input);
+			val = nn_2_val(calc_out);
+		} else {
+			gt = nn_2_val(input);
+			if (val == gt) {
+				++got;
+			}
+		}
+	}
+	printf("got %d/10000", got);
+	free(line);
+	fann_destroy(ann);
+	return 0;
+}
---
+++ train.c
@@ -0,0 +1,26 @@
+#include <fann.h>
+
+int main(void) {
+	const unsigned int num_input = 784;
+	const unsigned int num_output = 10;
+	const unsigned int num_layers = 3;
+	const unsigned int num_neurons_hidden = 30;
+	const float desired_error = (const float) 0.0005;
+	const unsigned int max_epochs = 1000;
+	const unsigned int epochs_between_reports = 1;
+
+	struct fann *ann = fann_create_standard(num_layers, num_input,
+			num_neurons_hidden, num_output);
+
+	fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
+	fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
+
+	fann_train_on_file(ann, "train.fann", max_epochs,
+			epochs_between_reports, desired_error);
+
+	fann_save(ann, "numbers.net");
+
+	fann_destroy(ann);
+
+	return 0;
+}
